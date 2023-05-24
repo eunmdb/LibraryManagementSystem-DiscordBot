b98348e01709a0ae250961ec7dd21c2cd06e39a2
eunmdb <32080056+eunmdb@users.noreply.github.com>
2023-05-23 18:53:39 -0700

    Update database.py

--- milestones/Milestone2/database.py
+++ milestones/Milestone2/database.py
@@ -2,34 +2,65 @@
 
 import os
 import pymysql.cursors
+# from dbmodels import 
 
-# note that your remote host where your database is hosted 
+# note that your remote host where your database is hosted
 # must support user permissions to run stored triggers, procedures and functions.
-db_host = os.environ["DB_HOST"] 
+db_host = os.environ["DB_HOST"]
 db_username = os.environ["DB_USER"]
 db_password = os.environ["DB_PASSWORD"]
 db_name = os.environ["DB_NAME"]
 
-def connect():
+class Database:
+  def connect(self=None):
     """
-    This method creates a connection with your database 
-    IMPORTANT: all the environment variables must be set correctly 
-               before attempting to run this method. Otherwise, it
-               will trown an error message stating that the attempt
-               to connect to your database failed.
-    """
+      This method creates a connection with your database 
+      IMPORTANT: all the environment variables must be set correctly 
+                 before attempting to run this method. Otherwise, it
+                 will trown an error message stating that the attempt
+                 to connect to your database failed.
+      """
     try:
-        conn = pymysql.connect(host=db_host,
-                               port=3306,
-                               user=db_username,
-                               password=db_password,
-                               db=db_name,
-                               charset="utf8mb4", cursorclass=pymysql.cursors.DictCursor)
-        print("Bot connected to database {}".format(db_name))
-        return conn
+      conn = pymysql.connect(host=db_host,
+                             port=3306,
+                             user=db_username,
+                             password=db_password,
+                             db=db_name,
+                             autocommit=True,
+                             charset="utf8mb4",
+                             cursorclass=pymysql.cursors.DictCursor)
+      print("Bot connected to database {}".format(db_name))
+      return conn
     except:
-        print("Bot failed to create a connection with your database because your secret environment variables " +
-              "(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) are not set".format(db_name))
-        print("\n")
-
-# your code here
+      print(
+        "Bot failed to create a connection with your database because your secret environment variables "
+        + "(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) are not set".format(db_name))
+      print("\n")
+  
+  
+  # your code here
+  def selectNoArgs(self,query):
+    conn = self.connect()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    conn.close()
+    return data
+  
+  def select(self,query,arguments = None):
+    conn = self.connect()
+    cursor = conn.cursor()
+    cursor.execute(query, arguments)
+    data = cursor.fetchall()
+    conn.close()
+    return data
+  
+  def insert(self,query, arguments):
+    try:
+      conn = self.connect()
+      cursor = conn.cursor()
+      cursor.execute(query, arguments)
+      return "Successfully added"
+    except:
+      print("The query failed")
+      return "Unsucessful command. Please check your arguments"
